e0a4df7f424c301f8f4b74b0715ab7028aa2e7a1
Helmy Agta Al Fatah <helmibaran20@gmail.com>
2019-01-19 21:34:30 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const navbar = document.getElementById('navbar');
+
+window.onscroll = function() {
+  var minScroll = window.innerHeight * 0.75;
+
+  if (window.innerWidth <= 720) {
+    minScroll = window.innerHeight * 0.25;
+  }
+
+  if (
+    window.scrollY >= minScroll && 
+    (window.innerHeight + window.scrollY) < document.body.offsetHeight
+  ) {
+    if (window.innerWidth <= 1280) {
+    navbar.classList.add('navbar__colored');      
+    } else {
+      navbar.classList.add('navbar__hide');
+    }
+  } else {
+    if (window.innerWidth <= 1280) {
+      navbar.classList.remove('navbar__colored');      
+    } else {
+      navbar.classList.remove('navbar__hide');
+    }
+  }
+}
